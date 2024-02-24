f546ffc0e147c524cc8e14b3b39f544ea1956721
georginacostilla <georginacostilla.gc@gmail.com>
2024-02-24 02:14:24 -0300

    se trabaja sobre el carousel del Home

---
+++ book-code/src/components/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../../../src/stylesheet/Home.css';
+import imgUno from '../../../src/img/libroEstrellasDelMundial.png';
+import imgDos from '../../../src/img/libroElHombreEnBuscaDeSentido.png';
+import imgTres from '../../../src/img/libroLaPsicologiaDelDinero.png';
+import imgCuatro from '../../../src/img/libroPensamientosNutritivos.png';
+import imgCinco from '../../../src/img/libroElDuelo.png';
+
+function Home() {
+    return (
+        <div className="containerDiseno">
+            <div class="disenoImg mt-3 mb-5">
+                <img src={imgUno} alt="libro: Estrellas del mundial" />
+                <img src={imgDos} alt="libro: El hombre en busca de sentido" />
+                <img src={imgTres} alt="libro: La psicologia del dinero" />
+                <img src={imgCuatro} alt="libro: Pensamientos nutritivos" />
+                <img src={imgCinco} alt="libro: El duelo" />
+            </div>
+        </div>
+    );
+}
+
+export default Home
