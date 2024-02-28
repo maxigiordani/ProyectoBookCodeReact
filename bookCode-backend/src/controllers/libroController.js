a4b22264a42e3b5c20a646cc71cf4ad709109631
Juan Lassalle <juanlassalle23@hotmail.com>
2024-02-28 16:32:48 -0300

    se trabajo sobre productdetail

--- bookCode-backend/src/controllers/libroController.js
+++ bookCode-backend/src/controllers/libroController.js
@@ -8,6 +8,19 @@ export const obtenerLibros = async (req,res) => {
    }
 }
 
+export const obtenerLibro = async (req,res) => {
+    try {
+        const {id} = req.body
+
+        const obtLibro = libro.filter(l => l.ID === id)
+
+        res.status(200).json(obtLibro)
+
+    } catch (error) {
+        res.status(500).json({mensaje: 'Error en el servidor'})
+    }
+}
+
 export const filtrarLibro = async (req,res) => {
     try {
         const {titulo, autor, genero} = req.query
